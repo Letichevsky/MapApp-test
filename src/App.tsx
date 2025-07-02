8d89ee641fb79d3e1adbfc190d53497f00596c87
VL <vladislavletichevsky@gmail.com>
2025-07-03 00:18:44 +0100

    create base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="w-full h-full min-h-screen bg-[#aa00ff] flex items-center justify-center"></div>
+    <div className="w-full h-full min-h-screen bg-[linear-gradient(to_bottom,_#333,_#222)] flex items-center justify-center"></div>
   );
 }
 
